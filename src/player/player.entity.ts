cc2f413bc3152034008490b936af5bab94048f52
bairon <fallasbaltodanobairon@gmail.com>
2025-08-28 09:36:50 -0600

    feat: ajustar la entidad Player para mejorar la definición de columnas y restricciones únicas

--- src/player/player.entity.ts
+++ src/player/player.entity.ts
@@ -1,13 +1,8 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, Unique } from 'typeorm';
 
-@Entity({ name: 'player' }) // nombre EXACTO en MySQL
+@Entity('player')
+@Unique(['name'])
 export class Player {
-  @PrimaryGeneratedColumn({ type: 'int', unsigned: true, name: 'id' })
-  id: number;
-
-  // Si ya existe UNIQUE en BD, evita decoradores que creen otro índice
-  // o nómbralos exactamente igual que en la BD:
-  // @Index('uq_player_name', ['name'], { unique: true })
-  @Column({ name: 'name', type: 'varchar', length: 30, nullable: false })
-  name: string;
+  @PrimaryGeneratedColumn() id: number;
+  @Column({ length: 30 }) name: string;
 }
